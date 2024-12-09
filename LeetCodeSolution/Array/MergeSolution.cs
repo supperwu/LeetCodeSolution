b52674e6624d02f83682e7a166376b2c60df2340
Jerry <supperwu4781@hotmail.com>
2024-12-09 15:38:54 +0800

    The solution for merging jagged array

---
+++ LeetCodeSolution/Array/MergeSolution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeSolution
+{
+    class IntervalComparer : IComparer<int[]>
+    {
+        public int Compare(int[] x, int[] y)
+        {
+            return x[0] - y[0];
+        }
+    }
+    public class MergeSolution
+    {
+        /*
+         * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+         * 请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+         * 
+         * 输入：intervals = [[1,4],[4,5]]
+            输出：[[1,5]]
+            解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+         */
+
+        public int[][] Merge(int[][] intervals)
+        {
+            if (intervals.Length == 0)
+            {
+                return new int[0][];
+            }
+
+            //二维数组排序
+            Array.Sort(intervals, new IntervalComparer());
+
+            var merged = new List<int[]>();
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                int L = intervals[i][0];
+                int R = intervals[i][1];
+
+                if (merged.Count == 0 || merged[merged.Count - 1][1] < L)
+                {
+                    merged.Add(new int[] { L, R });
+                }
+                else
+                {
+                    merged[merged.Count - 1][1] = Math.Max(merged[merged.Count - 1][1], R);
+                }
+            }
+
+            return merged.ToArray();
+        }
+
+        public void Run()
+        {
+            //交错数组
+            //int[][] intervals = new int[][] { 
+            //        new int[] { 1, 3 },
+            //        new int[] { 2, 6 },
+            //        new int[] { 8, 10 },
+            //        new int[] { 15, 18 },
+            //};
+
+            //简化的初始化
+            int[][] intervals = {
+                new int[]{ 1, 4},
+                new int[]{ 4, 5},
+            };
+
+          
+
+            int[][] merged = Merge(intervals);
+        }
+    }
+}
--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,58 @@ namespace LeetCodeSolution
     {
         static void Main(string[] args)
         {
-            new BinarySearch2Solution().Run();
+            int[,] array3 = new int[,] { { 1, 2 }, { 2, 3 }, { 5, 6 } };
+            int[,] array4 = { { 1, 2, 3 }, { 3, 4, 5 }, { 5, 6, 7} };
+
+            int[,] array5 = new int[3, 2];
+            int rows = array5.GetLength(0);
+            int cols = array5.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    array5[i, j] = i * j;
+                }
+            }
+
+            //交错数组 jagged array.
+            //直接初始化
+            int[][] array1 = new int[][] {
+                new int[]{ 1, 2},
+                new int[]{ 2, 3, 4},
+                new int[]{ 3, 4, 5, 6}
+            };
+
+            int columns = 0;
+            foreach (int[] row in array1)
+            {
+                if (row.Length > columns)
+                {
+                    columns = row.Length;
+                }
+            }
+
+            Console.WriteLine($"The jagged array's columns is:{columns}");
+
+            //简化初始化
+            int[][] array2 = {
+                new int[] { 1, 2 },
+                new int[] { 2, 3, 4},
+                new int[] { 3, 4, 5 ,6}
+            };
+
+            //循环初始化
+            int[][] array = new int[3][];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new int[i + 2];
+                for (int j = 0; j < i + 2; j++)
+                {
+                    array[i][j] = i * 2 + j + 1;
+                }
+            }
+
+            new MergeSolution().Run();
             Console.ReadLine();
         }
     }
